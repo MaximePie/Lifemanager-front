c53148eed356ec198a3a8e2ea4e2f9a42c385958
Chatmalow <maxime.pie.mail@gmail.com>
2022-01-16 07:47:11 +0100

    style(Product): Improve product display

--- src/components/molecules/Product.js
+++ src/components/molecules/Product.js
@@ -1,17 +1,19 @@
+import React from 'react';
+
 export default function Product({product, onUpdate, onDelete}) {
 
   const {quantity, name, _id: productId, isOK} = product;
 
   return (
     <div className={`Product ${isOK && 'Product--isOK'}`}>
-      <p>{quantity}</p>
-      <p>{name}</p>
+      <button onClick={() => {onDelete(productId)}}>🗑️</button>
+      <p className="Product__quantity">{quantity}</p>
+      <p className="Product__name">{name}</p>
       <input
         type="checkbox"
         onChange={(event) => onUpdate(productId, event.target.checked)}
         checked={isOK}
       />
-      <button onClick={() => {onDelete(productId)}}>Supprimer</button>
     </div>
   )
 }
