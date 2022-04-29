c85bcd31519a0bfd60e1fbbbc2c1e679a8d78e80
Chatmalow <maxime.pie.mail@gmail.com>
2022-04-29 08:35:35 +0200

    style(StyledComponents): prepare for styled components variables

---
+++ src/styles/styledComponentsVariables/_variables.ts
@@ -0,0 +1,13 @@
+const colors = {
+  success: '#b3ffb5',
+  danger: '#ffaba3',
+  primary: '#6ccdfb',
+};
+
+const spacings = {
+  small: '0.5em',
+  medium: '1em',
+  big: '2em',
+};
+
+export { colors, spacings };
