fde785ad30b2a2460ad0922ec1ed801a6412f629
Chatmalow <maxime.pie.mail@gmail.com>
2022-04-29 17:23:29 +0200

    feat(tasks): Add repetitive task feature

--- src/components/molecules/NewTaskForm.tsx
+++ src/components/molecules/NewTaskForm.tsx
@@ -4,6 +4,8 @@ import InputGroup from '../atoms/InputGroup';
 
 export default function NewTaskForm() {
   const [name, setName] = useState('');
+  const [isRepetitive, setRepetitionState] = useState(false);
+  const [delay, setDelay] = useState('0');
 
   return (
     <div className="NewTaskForm">
@@ -14,16 +16,34 @@ export default function NewTaskForm() {
         label="Nom"
         value={name}
       />
+      <label htmlFor="isRepetitive">
+        Répéter ?
+        <input type="checkbox" onChange={updateRepetitiveState} checked={isRepetitive} />
+      </label>
+      {isRepetitive && (
+        <label htmlFor="repetitionDelay">
+          Délai
+          <input type="text" onChange={updateDelay} value={delay} />
+        </label>
+      )}
       <button type="button" onClick={save}>Ajouter</button>
     </div>
   );
+
+  function updateDelay(event: ChangeEvent<HTMLInputElement>) {
+    setDelay(event.target.value);
+  }
+
+  function updateRepetitiveState(event: ChangeEvent<HTMLInputElement>) {
+    setRepetitionState(event.target.checked);
+  }
 
   function updateTaskName(event: ChangeEvent<HTMLInputElement>) {
     setName(event.target.value);
   }
 
   function save() {
-    postOnServer('/task', { name }).then(() => {
+    postOnServer('/task', { name, isRepetitive, delay: parseInt(delay, 10) }).then(() => {
       setName('');
     });
   }
